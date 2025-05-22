1613638c8e2c4b01ff1721aac9b1f0c52827af2e
yasir787 <yk9149711@gmail.com>
2025-05-21 18:30:46 -0700

    dynamic routing with flask

--- flask basic/app.py
+++ flask basic/app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 
 app = Flask(__name__) 
-@app.route('/') 
-def home(): 
-    return "hello world"
+@app.route('/user/<username>') 
+def home(username): 
+    return f"hello world {username}!"
 
 if __name__ == '__main__':
     app.run(debug=True)
