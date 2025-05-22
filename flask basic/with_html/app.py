ecf1153477ead2132827ec7a95628b18c57d31f2
yasir787 <yk9149711@gmail.com>
2025-05-21 19:23:28 -0700

    dynamic rendering wiht html

--- flask basic/with_html/app.py
+++ flask basic/with_html/app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return render_template ('index.html')
+@app.route('/hello/<name>')
+def home(name):
+    return render_template ('index.html',name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
